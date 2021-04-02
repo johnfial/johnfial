0eab2cea1a6b3a0dbbc9f49d59d001bff8794cbc
johnfial <12399190+johnfial@users.noreply.github.com>
2021-04-02 11:38:37 -0700

    practice ~30 (mostly reading)

--- cracking_the_coding_interview_v6_2020_book/ch01_arrays_and_strings_6_string_compression.py
+++ cracking_the_coding_interview_v6_2020_book/ch01_arrays_and_strings_6_string_compression.py
@@ -3,7 +3,7 @@
     # http://www.crackingthecodinginterview.com/solutions.html 
     # Amazon: https://www.amazon.com/Cracking-Coding-Interview-Gayle-McDowell/dp/0984782850/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=careercup-ctciwebsite-20&linkId=173f3d8878a1d7f0d131a85fbfc9f67f
     # Solutions GitHub: https://github.com/careercup/CtCI-6th-Edition
-# TODO STATUS:    
+# TODO STATUS:    Unfinished... 5-20 more min?
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 # 1.6
---
+++ cracking_the_coding_interview_v6_2020_book/ch01_arrays_and_strings_9_string_rotation.py
@@ -0,0 +1,39 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# by John Fial, 2021, https://github.com/johnfial/
+    # http://www.crackingthecodinginterview.com/solutions.html 
+    # Amazon: https://www.amazon.com/Cracking-Coding-Interview-Gayle-McDowell/dp/0984782850/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=careercup-ctciwebsite-20&linkId=173f3d8878a1d7f0d131a85fbfc9f67f
+    # Solutions GitHub: https://github.com/careercup/CtCI-6th-Edition
+# TODO STATUS:    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# 1.6 String Rotation
+# Assume you have a method isSubstring which checks if one word is a subscring of another. Given two strings, s1 and s2, 
+# write code to check if s2 is a rotation of s1 using only one call to isSubstring (eg "waterbottle" is a rotation of "erbottlewat")
+# Hints: 34, 88, 108
+
+def isSubscring(source_string, test_string):
+    if len(source_string) == len(test_string):
+        
+        print(source_string)
+        
+        
+        # do this
+        return
+    else:
+        print('lengths do not match. return False')
+        return False
+
+
+source_example_1 = 'waterbottle'
+test_example_1 = 'erbottlewat'
+
+isSubscring(source_example_1, test_example_1)
+
+
+source_example_2 = 'lollipops'
+test_example_2 = ''
+# isSubscring(source_example_2, test_example_2)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# by John Fial, 2021, https://github.com/johnfial/
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
