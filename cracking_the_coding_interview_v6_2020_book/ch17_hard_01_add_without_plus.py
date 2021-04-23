7824aeec0de6d7bdbecd09fb12e09d51a71ffbdd
johnfial <12399190+johnfial@users.noreply.github.com>
2021-04-23 09:23:00 -0700

    practice ~30

--- cracking_the_coding_interview_v6_2020_book/ch17_hard_01_add_without_plus.py
+++ cracking_the_coding_interview_v6_2020_book/ch17_hard_01_add_without_plus.py
@@ -3,36 +3,64 @@
 # http://www.crackingthecodinginterview.com/solutions.html 
 # Amazon: https://www.amazon.com/Cracking-Coding-Interview-Gayle-McDowell/dp/0984782850/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=careercup-ctciwebsite-20&linkId=173f3d8878a1d7f0d131a85fbfc9f67f
 # Solutions GitHub: https://github.com/careercup/CtCI-6th-Edition
-# TODO STATUS:    
-# TODO SUBMITTED: 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-
-# 17.1 Add without plus:
-# Write a function that adds two numbers. You should not use + or any other arithmetic operators.
-
-# IDEA: Think about a number line?
-# IDEA: 
-
-# HINTS: 466, 543, 600, 627, 641, 663, 691, 711, 723
-
-
-def add_without_operators(a, b):
-    sum = a
-    
-    print(sum)
-    print(a , b)
-
-a = 5
-b = 5
-add_without_operators(a, b)
-
-
-
-
+# TODO STATUS:    Started 22 April
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#   #   #   #   #   #   #   #   #   # Failed Attempts:  #   #   #   #   #   #   #   #   #   #
+
+
+# xx: Valid parenthesis:
+# Write a method to validate parenthesis within a string: i.e. that all are in order, matching, "balanced", etc.
+# NOTE list.sort() does NOT help me, for obvious reasons...
+
+example_1 = 'jello()' # valid
+example_2 = 'jello()()' # valid
+example_3 = 'jello(())'  # valid
+example_4 = 'jello(()())'  # valid
+example_5 = '(jello)()()' # valid
+
+example_6 = ')jello(' # invalid, even though even...
+example_7 = ')jello))' # invalid
+example_8 = 'jello)' # invalid
+example_9 = 'jello))((' # invalid
+
+def validate_parentheses(input_string):
+    parentheses_list = []
+    if type(input_string) == str:
+
+        for letter in input_string:
+            if letter == '(' or letter == ')':
+                parentheses_list.append(letter)
+
+        
+        if parentheses_list.count('(') == parentheses_list.count(')'):
+            print('number of open( == )closed parentheses! first step!')
+            # do more checking...
+            # loop through the first, which must be '('
+            # 2nd must be '(', starting a new check, OR ')', terminating that counter...
+            # variable counter_current_parentheses ?
+
+            print(f'parentheses_list: {parentheses_list}')
+            current_open_parentheses_counter = 0
+
+            # WORKING HERE # WORKING HERE # WORKING HERE # WORKING HERE # WORKING HERE 
+            for counter in range(parentheses_list.count('(')):
+
+                if parentheses_list[counter] == '(':
+                    print('pass 2nd check! # do some work!')
+                    if parentheses_list[counter+1] == ')':
+                        print('MATCH! super balanced')
+            # WORKING HERE # WORKING HERE # WORKING HERE # WORKING HERE # WORKING HERE 
+
+        else: print('numbers do not match, odd number fails!')
+
+    else:  # not a string...
+        print('invalid datatype input!')
+        return False
+
+validate_parentheses(example_2)
+
+
 
 
 
---
+++ cracking_the_coding_interview_v6_2020_book/test2.py
@@ -0,0 +1,17 @@
+a = 'cat'
+b = 'tac2'
+
+a = list(a)
+b = list(b)
+
+
+print(a)
+
+a.sort()
+b.sort()
+
+print(a)
+print(b)
+
+if a == b:
+    print('horray!')
