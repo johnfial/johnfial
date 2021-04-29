fe598c91ca7e8d1c25803d6000c4a837ca5ac5bf
johnfial <12399190+johnfial@users.noreply.github.com>
2021-04-29 09:43:47 -0700

    practice 30

---
+++ cracking_the_coding_interview_v6_2020_book/ch06_xx_egg_drop.py
@@ -0,0 +1,35 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# by John Fial, 2021, https://github.com/johnfial/
+# http://www.crackingthecodinginterview.com/solutions.html 
+# Amazon: https://www.amazon.com/Cracking-Coding-Interview-Gayle-McDowell/dp/0984782850/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=careercup-ctciwebsite-20&linkId=173f3d8878a1d7f0d131a85fbfc9f67f
+# Solutions GitHub: https://github.com/careercup/CtCI-6th-Edition
+
+# TODO STATUS:     finished 20 apr?
+# think I'm done, check for edge cases
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# 16.xx Egg Drop
+# Hints Used:       156, 233, 294,
+# Hints Remaining:  333, 357, 374, 395
+
+import random
+
+def egg_drop_test(max_floors=100, eggs=100):
+    print(random.randint(1, max_floors))
+
+    random.choice()
+
+    return
+
+eggs = 2
+egg_drop_test(100, eggs)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#   #   #   #   #   #   #   #   #   # Failed Attempts:  #   #   #   #   #   #   #   #   #   #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# by John Fial, 2021, https://github.com/johnfial/
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
