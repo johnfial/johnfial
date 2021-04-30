9d51ffb9d7f7d8f089b68fd0fbece3c476cb37e7
johnfial <12399190+johnfial@users.noreply.github.com>
2021-04-30 10:27:07 -0700

    practice 15

--- 101 & 102 TA For PDX Code Guild Intro Classes/Originals, Aug 2020/programming_101/unit_3/2021_unit_3_lab_grading.py
+++ 101 & 102 TA For PDX Code Guild Intro Classes/Originals, Aug 2020/programming_101/unit_3/2021_unit_3_lab_grading.py
@@ -1,52 +1,77 @@
 # https://github.com/PdxCodeGuild/Programming101/blob/master/labs/grading.md
 # if and elif statements
-# input grade, then compare that grade to a randomint() of another student
+# input user_grade, then compare that user_grade to a randomint() of another student
 # then output if they passed or not
 import math
 import random
 
-def main():
-    # grade = input('''Please input your grade. 
-    # Remeber that falsifications are punishable by up to a $5000 fine and/or 2-years imprisonment.
-    # \nGrade> ''')
-    grade = 75
+def convert_numerical_to_letter_score(numerical_score):
+    # print(f'function convert_numerical_to_letter_score() with input: {numerical_score}')
 
-    grade = int(grade)
-    if grade >= 90 and grade <= 100:
-        print('Congratulations, you received an \'A\'! You must have studied hard!')
-    elif grade >= 80 and grade <= 89:
-        print('Congratulations, you received a \'B\'! Passing by all measures, but not to the point where you annoy any classmates!')
-    elif grade >= 70 and grade <= 79:
-        print('''
-        Congratulations, you received a \'C\'! 
-        You passed, and you\'re either dissapointed in yourself for the \'low\' score, 
-        or thrilled that you passed! If the former, may I suggest you cheer up by 
-        spending more time with the underachievers *thrilled* with their \'Cs\'!''')
-    elif grade >= 60 and grade <= 69:
-        print('You received a \'D\'. Not much to see here, folks. . .')
-    elif grade <= 59:
-        print('Wow, you received an \'F\'. Bummer.')
+    numerical_score = int(numerical_score)
+
+    if numerical_score == 100:
+        letter_score = 'A+'
+        return letter_score
+    elif 90 <= numerical_score <= 99:
+        letter_score = 'A'
+    elif 80 <= numerical_score <= 89:
+        letter_score = 'B'
+    elif 70 <= numerical_score <= 79:
+        letter_score = 'C'
+    elif 60 <= numerical_score <= 69:
+        letter_score = 'D'
+    elif numerical_score <= 59:
+        letter_score = 'F'
+        return letter_score
     else:
-        print('input error')
+        letter_score = 'input error'
+        return letter_score
+
+    appendix = ''
+    if numerical_score % 10 <= 3:
+        appendix = '-'
+    elif numerical_score % 10 >=7:
+        appendix = '+'
+    else:
+        appendix = ''
+
+    letter_score += appendix
+    return letter_score
+
+def main():
+    user_grade = input('''Please input your user_grade. 
+    Remeber that falsifications are punishable by up to a $5000 fine and/or 2-years imprisonment.
+    \nuser_grade> ''')
+    # user_grade = 72  # testing
+    user_grade = int(user_grade)
+
+    if user_grade >= 90 and user_grade <= 100:
+        user_statement = 'Congratulations, you must have studied hard!'
+    elif user_grade >= 80 and user_grade <= 89:
+        user_statement = 'Congratulations! Passing by all measures, but not to the point where you annoy any classmates!'
+    elif user_grade >= 70 and user_grade <= 79:
+        user_statement = '''Congratulations! You passed, and you\'re either dissapointed in yourself for the \'low\' score, or thrilled that you passed! 
+    If the former, may I suggest you cheer up by spending more time with the underachievers *thrilled* with their \'Cs\'!'''
+    elif user_grade >= 60 and user_grade <= 69:
+        user_statement = 'Not much to see here, folks. . .'
+    elif user_grade <= 59:
+        user_statement = 'Wow. Bummer.'
+    else:
+        user_statement = 'input error'
 
     rival_score = random.randint(0,100)
-    if grade > rival_score:
-        print(f'An anonymous rival scored a {rival_score}. Horray, you beat their score!')
-    elif grade == rival_score:
-        print(f'An anonymous rival scored a {rival_score}. You tied their score -- exactly!')
-    elif grade < rival_score:
-        print(f'An anonymous rival scored a {rival_score}. Unfortutately, you did not surpass their score.')
-
-    rival_appendix = ''
-    if rival_score % 10 <= 3:
-        rival_appendix = '-'
-    elif rival_score % 10 >=7:
-        rival_appendix = '+'
+    if user_grade > rival_score:
+        rival_statement = 'Horray, you beat their score!'
+    elif user_grade == rival_score:
+        rival_statement = 'You tied their score -- exactly!'
+    elif user_grade < rival_score:
+        rival_statement = 'Unfortutately, you did not surpass their score.'
 
     final_output = f'''
-    {rival_appendix}
+    {user_grade}/100 is a {convert_numerical_to_letter_score(user_grade)}. {user_statement}
+    With a {rival_score}/100, your rival scored a {convert_numerical_to_letter_score(rival_score)}. {rival_statement}    
     '''
-    # BUG  100 printed wrong!
     print(final_output)
 
 main()
--- 101 & 102 TA For PDX Code Guild Intro Classes/Originals, Aug 2020/programming_101/unit_4/2021_apr_lab_06_rock_paper_scissors.py
+++ 101 & 102 TA For PDX Code Guild Intro Classes/Originals, Aug 2020/programming_101/unit_4/2021_apr_lab_06_rock_paper_scissors.py
@@ -3,71 +3,67 @@
 import random
 import time
 
-def game(rounds=999):
-    print('Welcome to rock, paper, scissors 2021 v2.2.20210305!')
+def rock_paper_scissors():
+    print('Welcome to rock, paper, scissors 2021 v2.3.20210429!')
 
     current_round = 1
     user_wins = 0
     user_losses = 0
 
-    while current_round <= rounds:
-        print(f'''~~~~~~~~~~~~~~~~~~~~~~~ Round {current_round} of {rounds}: ''', end="")
+    while True:
+        print(f'''Round {current_round}: ''', end="")
 
-        choices_list = ['rock', 'paper', 'scissors',]
+        choices_list = ['rock', 'paper', 'scissors', 'quit']
 
-        print('Please type from the following three choices: ')
+        print('Please type from the following four choices: ')
         for choice in choices_list:
             print(choice)
-        user_choice = input('~~~~~~~~~~~~~~~~ > ')
+        user_choice = input('~~~~~~~~~~~~~~~~ input > ')
 
-        computer_choice = random.choice(choices_list)
-        print(f'The computer casts. . .')
-        time.sleep(1)
-        print(f'. . .{computer_choice.upper()}! ', end="")
-        # print()
-
-        # firure out how to wait the text...
 
         if user_choice in choices_list:
+            if user_choice == 'quit':
+                break
+            
+            computer_choice = random.choice(choices_list[0:2]) # slice off the 'quit' choice!
+            print(f'The computer casts. . .')
+            time.sleep(1)
+            print(f'{computer_choice.upper()}! ') # , end="")
+
             if user_choice == 'rock':
                     if computer_choice == 'scissors':
                         print('Your rock smashes feeble scissors and you win!')
                         user_wins += 1
+                        current_round += 1
                     elif computer_choice == 'paper':
                         print('Nobody knows why paper wraps rock, but it does. You lose!')
                         user_losses += 1
+                        current_round += 1
             if user_choice == 'paper':
                     if computer_choice == 'rock':
                         print('Nobody knows why paper wraps rock, but it does. You win!')
                         user_wins += 1
+                        current_round += 1
                     elif computer_choice == 'scissors':
                         print('You lose!')
                         user_losses += 1
+                        current_round += 1
             if user_choice == 'scissors':
                     if computer_choice == 'paper':
                         print('You cut and win!')
                         user_wins += 1
+                        current_round += 1
                     elif computer_choice == 'rock':
                         print('Your scissors are broken to bits! You lose!')
                         user_losses += 1
+                        current_round += 1
             if user_choice == computer_choice:
                 print('You tied! Statistically very likely but no less boring!')
+                current_round += 1
         else:
             print('Invalid entry!')
 
-        current_round += 1
+    # BUG division by 0 if they don't play...
+    print(f'TOTALS: wins: {user_wins} losses: {user_losses}.') # Win percentage: {user_wins/user_losses}')
 
-    print(f'TOTALS: wins: {user_wins} losses: {user_losses}.') #  Win percentage: {user_wins/user_losses}')
-
-game(4)
-
-
-
-
-# You could do nested ifs for each choice
-# if user has rock
-#     if computer has paper
-#     if computer has scissors
-# The final advanced version talks about using a dictionary and that's really the best way to simplify this one.
-# https://hackernoon.com/how-to-approach-any-coding-problem-9230f3ad6f9
-# This article https://medium.com/@dannysmith/breaking-down-problems-its-hard-when-you-re-learning-to-code-f10269f4ccd5 uses Ruby instead of Python, but the process is the same for thinking about the problems (edited)
+rock_paper_scissors()
