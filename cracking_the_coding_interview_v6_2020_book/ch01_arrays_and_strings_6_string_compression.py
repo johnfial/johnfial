1adfd96a7f57c5dd17792290d3bef5bac2a962df
johnfial <12399190+johnfial@users.noreply.github.com>
2021-05-05 11:03:57 -0700

    practice ~10

--- cracking_the_coding_interview_v6_2020_book/ch01_arrays_and_strings_6_string_compression.py
+++ cracking_the_coding_interview_v6_2020_book/ch01_arrays_and_strings_6_string_compression.py
@@ -3,7 +3,9 @@
     # http://www.crackingthecodinginterview.com/solutions.html 
     # Amazon: https://www.amazon.com/Cracking-Coding-Interview-Gayle-McDowell/dp/0984782850/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=careercup-ctciwebsite-20&linkId=173f3d8878a1d7f0d131a85fbfc9f67f
     # Solutions GitHub: https://github.com/careercup/CtCI-6th-Edition
-# TODO STATUS:    Unfinished... 5-20 more min?
+
+# TODO STATUS:    Unfinished... 5-10 more min? Just need to replace the consecutive letters with the count...
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 # 1.6
@@ -23,15 +25,25 @@ def compress_string(input_string):
     c2 = 1
 
     while c1 < len(input_string)-1:
-        
-        # if counters are the same, increment letter counter
-        print(f'{input_string[c1]} + {input_string[c2]}')
-        if input_string[c1] == input_string[c2]:
-            print('yes')
 
-            # replace:
-            # while loop? then at end, replace range of counters by # seen
-        
+        # print(f'{input_string[c1]} + {input_string[c2]}')
+
+        temp_counter = 1
+        temp_c2 = c2
+        try:  # this is clunky and kind of cheating!
+            while input_string[c1] == input_string[c2]:
+                print(f'input_string[c1]=input_string[c2], input_string[{c2}] = {input_string[c2]}')
+                
+                temp_counter += 1
+                c2 += 1
+        except:
+            pass
+
+        print(f'the letter {input_string[c1]} appeared temp_counter: {temp_counter} times')
+        # take the range of those counters, SLICE the string, and replace with letter[counter]
+        print(f'{input_string[c1:c2]}')
+
+        c2 = temp_c2  # reset c2
         c1 += 1
         c2 += 1
 
