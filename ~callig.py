e4d366560df9c114316217f56912b64cc4b6ade8
johnfial <12399190+johnfial@users.noreply.github.com>
2021-05-06 21:50:42 -0700

    practice ~15, tired...

--- ~callig.py
+++ ~callig.py
@@ -1,10 +1,13 @@
-# goal: make a random string of letters, numbers, or a random word list to practice calligraphy
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# I started practicing calligraphy, and wanted a file to give me random words to
+# practice, plus random letters. So that's this!
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 import string
 import random
 
-# below @ https://pypi.org/project/english-words/ :
-from english_words import english_words_set
+from english_words import english_words_set  # from https://pypi.org/project/english-words/
+
 english_list = list(english_words_set)
 
 letters_upper = list(string.ascii_uppercase)
@@ -15,17 +18,25 @@ def print_words(max=5):
     for i in range(max):
         integer = random.randint(0, 25)
         
+        print(f'~~~~~~~~~~~~ PRACTICE #{i+1}/{max}: ~~~~~~~~~~~~~~')
         print((english_list[integer].upper() + ' ') * 6)
         print(letters_upper[integer] * 20)
+    print(f'~~~~~~~~~~~~ ALL DONE! ~~~~~~~~~~~~~~~~~~~')
 
 
-print_words(4)
+print_words(4) # change this
 
-# print(len(english_words_set)) # 25,000 words!
+# print entire word set: WARNING: 25,000 words!
+# print(len(english_words_set))
+# print(english_words_set)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# poetry naturally flows out when practicing calligraphy, so here's some random stuff:
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-# Random poems:
 # One minute to go, one life to share
 # The road hard to walk is so often bare
 
