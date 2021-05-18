ee147eceb6edcff036197a80d5e19a4b9e7b563f
johnfial <12399190+johnfial@users.noreply.github.com>
2021-05-18 12:04:47 -0700

    practice ~10 ayer

---
+++ colors_test.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Back, Style
+print(Fore.RED + 'this is red!')
+print(Fore.BLUE + 'this is blue')
+print(Style.BRIGHT)
+print('test color')
+print(Fore.RESET)
+print(Style.RESET_ALL)
+
+print('test')
+# from colorama import Fore, Back, Style
+# print(Fore.RED + 'this is red text')
+# print(Back.BLUE + 'this is red text on a blue background')
+# print(Style.RESET_ALL)
+# print('back to normal')
+
