60c35ac418c8b15555fe15fd8a6d637c44ec38ea
johnfial <12399190+johnfial@users.noreply.github.com>
2021-06-09 11:28:50 -0700

    pdf play

---
+++ cracking_the_coding_interview_v6_2020_book/ch10_01_sorted_merge.py
@@ -0,0 +1,7 @@
+
+
+# 10.1 Sorted Merge: You are given two sorted arrays, A and B, where A has a large enough buffer 
+# at the end to hold B. Write a message to merge B into A in sorted order.
+# Hints: 332
+
+
--- random_misc/pdf_output.py
+++ random_misc/pdf_output.py
@@ -2,11 +2,11 @@
 
 from fpdf import FPDF
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# make the first object/file
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=50)
-
-
 pdf.cell(200, 10, txt = 'test this',
     ln = 1, align = 'C')
 
@@ -16,4 +16,25 @@ for x in range(10):
 pdf.cell(200, 10, txt = line_2, ln = 2, align = 'C')
 
 pdf.output("test.pdf")
-print('finished~~~~~~~~~~~~~')
+print('finished 1~~~~~~~~~~~~~')
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # print a 2nd object/file
+pdf2 = FPDF()
+pdf2.add_page()
+pdf2.set_font("Arial", size=50)
+pdf2.cell(200, 10, txt = 'page2',
+    ln = 1, align = 'C')
+
+line_2 = ''
+for x in range(5):
+    line_2 += (str(x) + 'xxx ')
+pdf2.cell(200, 10, txt = line_2, ln = 2, align = 'C')
+
+pdf2.output("test2.pdf")
+print('finished 2~~~~~~~~~~~~~')
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# now let's try to edit the first and output again
+import time
+time.sleep(1) # change this 
