49c50aee0d361b249933e7096b41fd20bd8cf739
johnfial <12399190+johnfial@users.noreply.github.com>
2021-06-15 16:16:12 -0700

    practice

--- cracking_the_coding_interview_v6_2020_book/ch10_02_group_anagrams.py
+++ cracking_the_coding_interview_v6_2020_book/ch10_02_group_anagrams.py
@@ -1,8 +1,7 @@
-
-
 # 10.2 Group Anagrams: Write a method to sort an array of strings so that all the anagrams
 # are next to each other.
 # Hints: 177, 182, 263, 342
+# NOTE STATUS: Currently grouping by length, not yet by anagram...
 
 a = ['string', 'pony', 'four', 'furo', 'alphabet', 'ginrst', 'yopn', 'testing']
 
---
+++ cracking_the_coding_interview_v6_2020_book/ch10_06_sort_big_file.py
@@ -0,0 +1,13 @@
+# 10.6 Sort Big File: Imagine you have  a 20GB file with one string per line. Explain how you would sort the file. 
+# Hints: 207
+
+a = ['string', 'pony', 'four', 'furo', 'alphabet', 'ginrst', 'yopn', 'testing']
+
+def sort_big_file(input):
+    print(input)
+
+sort_big_file(a)
+# NOTE WHITEBOARDING:
+# First breakup lines into strings inside array...
+# sort array by length
+# if len(string) == itself, sort by alphabetically?
