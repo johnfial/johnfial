858245a9ffbd9d4c4df1ca99d2695cea359ba57a
johnfial <12399190+johnfial@users.noreply.github.com>
2021-06-29 11:15:28 -0700

    js

---
+++ javascript_play/javascript.js
@@ -0,0 +1,18 @@
+// tailwind
+
+// GIT BRANCHES NOTES:
+// create file
+// https://github.com/PdxCodeGuild/class_salmon/blob/main/0%20Intro/03%20Git.md
+// git checkout lets you switch to new branch
+// git checkout -b branchname
+// add / commit
+// git push origin branch_name
+// then have to do a pull request WHEN it's final...
+
+// .
+// .
+// // 
+  
+ 
+
+
