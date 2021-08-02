6e92e313c6ce6b181938c7ace343d79c2046c640
johnfial <12399190+johnfial@users.noreply.github.com>
2021-08-02 10:09:56 -0700

    weekend update!

---
+++ play_django/django_API_example/notes.py
@@ -0,0 +1,6 @@
+# notes from library example monday 2 aug 2021:
+
+# make books app
+# class Book
+    # with title, subtitle, author, isbn (char field and not int)
+    
---
+++ random_misc/python_boolean_play.py
@@ -0,0 +1,8 @@
+def true_false():
+    True = False
+    False = True
+    
+    print(True)
+
+print(true_false)
+true_false()
