774be0e8547b470121adc939416f43c05974875b
johnfial <12399190+johnfial@users.noreply.github.com>
2022-12-12 08:37:39 -0800

    Update 2022.py

--- 2022.py
+++ 2022.py
@@ -6,20 +6,12 @@
 print('*' * 50)
 
 example = '''
-1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000'''
+2494
+8013
+1055
+5425
+9104
+10665'''
 # each elf inventory sep by newline
 # so which elf is carrying the MOST calories? 
 
@@ -27,19 +19,31 @@ example = '''
 # populate with each number
 # 2d array?
 
-print_output = example.split('\n')
-print(print_output)
+calories_list = example.split('\n')
 
 counter = -1
 elves_calories = []
 
-for item in print_output:
+for calorie in calories_list:
 
-    if item == '':
+    if calorie == '':
         elves_calories.append(0)
         counter += 1
-    print(elves_calories, item, '***aoeu') # ERROR HERE trying to add int of '', either remove this or...
-    print(item)
-    elves_calories[counter] += int(item)
-    print(item)
-print(elves_calories)
+        continue
+    # print(elves_calories, calorie, '***aoeu') # ERROR HERE trying to add int of '', either remove this or...
+    # print(calorie)
+    elves_calories[counter] += int(calorie)
+    # print(calorie)
+# print(elves_calories)
+print(max(elves_calories))
+maximum = 0
+print(maximum)
+maximum += elves_calories.pop(elves_calories.index(max(elves_calories)))
+print(maximum)
+maximum += elves_calories.pop(elves_calories.index(max(elves_calories)))
+print(maximum)
+maximum += elves_calories.pop(elves_calories.index(max(elves_calories)))
+print(maximum)
+# 211107 IS WRONG, TOO HIGH
+
+# REMEMBER ACTUAL DATA -- NOT EXAMPLE!
