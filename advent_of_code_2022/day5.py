fb60c7e282d4bb1b6774c8922ce9f88ca3890b59
Fial <12399190+johnfial@users.noreply.github.com>
2023-01-20 19:16:50 -0800

    minor, but files moved into double folders?

--- advent_of_code_2022/day5.py
+++ advent_of_code_2022/day5.py
@@ -51,20 +51,46 @@ The Elves just need to know which crate will end up on top of each stack; in thi
 
 After the rearrangement procedure completes, what crate ends up on top of each stack?'''
 
-# # # example:
-# list_of_assignments = ['2-4,6-8', '2-3,4-5', '5-7,7-9', '2-8,3-7', '6-6,4-6', '2-6,4-8']
-# list_of_assignments = example_1.split('\n')
-# # print(list_of_assignments)  # ['2-4,6-8', '2-3,4-5', '5-7,7-9', '2-8,3-7', '6-6,4-6', '2-6,4-8']
+# https://adventofcode.com/2022/day/5
+
+# STEP 1: convert the moves into a list of moves)
+
+# # example:
+example_moves = '''move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2'''
+example_moves = example_moves.split('\n')
+print(example_moves)
+list_of_moves = example_moves
 
 # # actual:
-# file_to_open = 'day4.txt'
+# file_to_open = 'day5.txt'
 # with open(file_to_open, 'r') as file:
 #     input = file.read()
-# list_of_assignments = input.split('\n')
+# list_of_moves = input.split('\n')
+# print(len(list_of_moves))
+# # actual list starts at line 11, so slice off index 0 through 10:
+# list_of_moves =  list_of_moves[:10]
+# # test print to see if it sliced properly
+# print(list_of_moves[0:10])
+
+for i in range(len(list_of_moves)):
+    temp = list_of_moves[i]
+    temp = temp.split(' ') # ['move', '1', 'from', '2', 'to', '1'] # len = 6, index 0 through 5
+    # SUBTRACT 1, BECAUSE THE INPUT USES HUMAN NUMBERS, but we want an index
+    temp = (int(temp[1]), int(temp[3])-1, int(temp[5])-1) # (number_to_move, stack_source, stack_dest)
+    list_of_moves[i] = temp
+    # print(temp, '<<<< (number_to_move, stack_source, stack_dest)')
+
+print(list_of_moves)
+    
 
 
-# https://adventofcode.com/2022/day/5
-# 1 convert the first X lines into the lists
+
+
+# STEP 2: convert the first X lines into the lists
+
 example_boxes = '''
     [D]    
 [N] [C]    
@@ -79,23 +105,80 @@ example_boxes = '''
 
 # print(f'counter = {counter}')
 
-# 2 create a practice list manually
-
+# 2 create a practice list manually:
 boxes_list = [
-    ['N', 'Z'],
-    ['D', 'C', 'M'],
+    ['Z', 'N'],
+    ['M', 'C', 'D',],
     ['P']
 ]  # outer array from left to right, inner array: starting from bottom box, with top box as higher index
+# boxes_list = [['N', 'Z', 'one'], ['D', 'C', 'M'], ['P', 'a', 'b']]
+
+def rotate(array):
+    # quicker -- lookup how to rotate a printed map/grid/array in python?
+    
+    # METHOD ONE -- # rotate try 1: -- https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
+    
+    # temp_list = list(zip(*boxes_list[::-1]))
+    # boxes_list = temp_list
+    # print(boxes_list)
+
+    # METHOD TWO
+
+    # # rotate alternate way (opposite direction)
+    # rotated = list(reversed(list(zip(*boxes_list))))
+    rotated = list(zip(*boxes_list))[::-1]
+    return rotated
+
+# print(boxes_list)
+# boxes_list = rotate(boxes_list)
+# print(boxes_list)
 
 # print the box output:
-for i in range(len(boxes_list)):
-    line = ''
-    for item in boxes_list[i]:
-        line += f'[{item}]'
-
+def print_output(boxes_list_input):
+    for i in range(len(boxes_list_input)):
+    
+        line = ''
+        for item in boxes_list_input[i]:
+            line += f'[{item}]'
+    line = f' '
+    
+    for i in range(len(boxes_list_input)):
+        line += f' {i} '
+    
     print(line)
 
+# print_output(boxes_list)
 
-# quicker -- lookup how to rotate a printed map/grid/array in python?
+# STEP 3: move the boxes!
 
-# convert the other lines into the moves... tuples in an array, maybe? then loop over the array?
+print(boxes_list)
+# convert the other lines into the moves... tuples in an array, maybe? then loop over the array? 
+# use a STACK, which is just a list in python using only .pop() and .append()
+
+'''
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 '''
+ 
+'''(number_to_move, stack_source, stack_dest)'''
+(1, 2, 1)
+
+print('*' * 50)
+
+for move_index in range(len(list_of_moves)):
+
+    # unpack
+    move_tuple = list_of_moves[move_index]
+    number_to_move, stack_source, stack_dest = move_tuple
+
+    # loop over number
+    for x in range(number_to_move):
+
+        # pop one off source stack...
+        temp_box = boxes_list[stack_source].pop()
+        # ... append to dest stack
+        boxes_list[stack_dest].append(temp_box)
+        # print(boxes_list)
+
+print(boxes_list)
